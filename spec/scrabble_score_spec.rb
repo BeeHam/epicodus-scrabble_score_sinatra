8cf366d073377f38991570a3b247828ee3107ba1
Justin Scott & Brigham Thompson <student@epicodus.com>
2015-08-04 12:03:43 -0700

    Second spec

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -5,4 +5,8 @@ describe('String#scrabble_score') do
   it('splits string into an array') do
     expect(("scrabble").scrabble_score()).to(eq(["s","c","r","a","b","b","l","e"]))
   end
+
+  it("returns a value for the one point letters") do
+    expect(("a").scrabble_score()).to(eq(1))
+  end
 end
