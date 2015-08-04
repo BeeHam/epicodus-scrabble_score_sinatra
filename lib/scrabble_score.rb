ec71c3646452a9f799e9edbb663fa79a20859266
Justin Scott & Brigham Thompson <student@epicodus.com>
2015-08-04 12:26:55 -0700

    Second spec pass used hash

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,5 +1,13 @@
 class String
   define_method(:scrabble_score) do
-    self.split("")
+    one_pointers = ['a','e','i','o','u','l','n','r','s','t']
+    score = {one_pointers => 1}
+    split_word = self.split("")
+
+    if score.include?(one_pointers)
+      score.fetch(one_pointers)
+    else
+      "Didn't work"
+    end
   end
 end
