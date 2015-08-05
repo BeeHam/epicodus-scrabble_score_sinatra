8d1feb46726be69925d49444faac2555e2ef9cbc
Vaughn Dice & Brigham Thompson <student@epicodus.com>
2015-08-05 10:11:04 -0700

    commenting out unused specs in scrabble_score_spec

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -6,13 +6,13 @@ describe('String#scrabble_score') do
   #   expect(("scrabble").scrabble_score()).to(eq(["s","c","r","a","b","b","l","e"]))
   # end
 
-  it("returns a value for the one point letters") do
-    expect(("a").scrabble_score()).to(eq([1]))
-  end
-
-  it("assigns a value to every letter in the word") do
-    expect(("word").scrabble_score()).to(eq([4,1,1,2]))
-  end
+  # it("returns a value for the one point letters") do
+  #   expect(("a").scrabble_score()).to(eq([1]))
+  # end
+  #
+  # it("assigns a value to every letter in the word") do
+  #   expect(("word").scrabble_score()).to(eq([4,1,1,2]))
+  # end
 
   it("tallies letter values into score") do
     expect(("word").scrabble_score()).to(eq(8))
