1ba6b86a0a7d87550c1e9f3f52f768cd568f88b6
Vaughn Dice & Brigham Thompson <student@epicodus.com>
2015-08-05 11:40:26 -0700

    Added a return to home button

--- spec/scrabble_score_integration_spec.rb
+++ spec/scrabble_score_integration_spec.rb
@@ -10,4 +10,10 @@ describe('the scrabble score path', {:type => :feature}) do
     click_button('Send')
     expect(page).to have_content(8)
   end
+
+  it('can return users to index from scrabble score page') do
+    visit('/score?score=word')
+    click_link('Return')
+    expect(page).to have_content('Welcome to your Scrabble score calculator!')
+  end
 end
